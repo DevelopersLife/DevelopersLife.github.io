533ebc58d8c3d01de97c44a72cdd35bcd9890282
Mich Murabito <mich.murabito@gmail.com>
2022-08-27 17:11:40 +0200

    remove cache

--- .next/static/BxaE1B1DGYQDk2qi3UNKQ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002F","\u002Ftags","\u002Fchannel\u002F[channel]","\u002Fvideo\u002F[...slug]","\u002Ftags\u002F[tag]","\u002Fvideo\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
