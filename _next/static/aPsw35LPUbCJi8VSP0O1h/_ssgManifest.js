edb7173117eeb639ff40749bdc2067895ceeaab8
akelity <akelity@users.noreply.github.com>
2022-08-27 15:05:23 +0000

    deploy: a2763b040b113ee0b9ece5a61022580150405afd

---
+++ _next/static/aPsw35LPUbCJi8VSP0O1h/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftags","\u002Fabout","\u002F","\u002Fchannel\u002F[channel]","\u002Ftags\u002F[tag]","\u002Fvideo\u002Fpage\u002F[page]","\u002Fvideo\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
